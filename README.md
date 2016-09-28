72b76108965f0db8c4e9864b4aa9501a56041b33
Alexander <alexander.zybin@gmail.com>
2016-09-28 15:52:47 +0300

    201609281552

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-"# gulp-pagebuilder2" 
+# [gulp](https://github.com/wearefractal/gulp)-pagebuilder
+
+Pagebuilder2 is a build and real time include engine for HTML, JavaScript, CSS and in general any type of text file that you wish to might want to "include" other files into.
+It is modification of gulp-pagebuilder
+
+## Install
+
+Install with [npm](https://npmjs.org/package/gulp-pagebuilder2).
+
+```
+npm install --save-dev gulp-pagebuilder2
+```
+
+## Examples
+
+```js
+var gulp = require('gulp'),
+	pagebuilder2 = require('gulp-pagebuilder2'));
+
+gulp.task('default', function () {
+	gulp.src('src/*.html')
+		.pipe(pagebuilder2('src'))
+		.pipe(gulp.dest('build/'));
+});
+```
+
+## How use it in sources
+
+```html
+<div class="someclass" >
+	[[azbntple tpl="some/block/in/src/somefile.html" class="foo bar" ]]
+</div>
+```
+
+## Content of some/block/in/src/somefile.html
+
+```html
+<div class="otherclass {{class}}" >
+	Some content
+</div>
+```
+
+## Result after compile
+
+```html
+<div class="someclass" >
+	<div class="otherclass foo bar" >
+		Some content
+	</div>
+</div>
+```
---
+++ index.js
@@ -0,0 +1,57 @@
+var	fs = require('fs'),
+	path = require('path'),
+	through = require('through2'),
+	gutil = require('gulp-util'),
+	azbntple = require('azbn-tple')
+	;
+
+const PLUGIN_NAME = 'gulp-pagebuilder2';
+
+function pageBuilder2(root) {
+	
+	root = __dirname + '/../../' + (root || '');
+	root = path.normalize(root);
+	
+	var tple = new azbntple({
+		part_path : root,
+		cache : {
+			tpls : [
+				//'tpl.html',
+			],
+		},
+	})
+	
+	return stream = through.obj(function(file, enc, cb) {
+		if (file.isStream()) {
+			//this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+			return cb();
+		}
+		
+		if (file.isBuffer()) {
+			//file.contents = Buffer.concat([prefixText, file.contents]);
+			
+			var code = file.contents.toString();
+			
+			tple.parseStr(code, {}, function(err, res_str){
+				file.contents = new Buffer(res_str, 'utf8');
+				
+				this.push(file);
+				
+				cb();
+			});
+			
+		} else {
+			
+			file.contents = new Buffer(code, 'utf8');
+			
+			this.push(file);
+			
+			cb();
+			
+		}
+		
+	});
+	
+}
+
+module.exports = pageBuilder2;
